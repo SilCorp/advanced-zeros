43e99e4868518f01e61b07802a20fe9b3fa4f17e
Alexander Silchenko <san4ys.1998@gmail.com>
2019-03-24 23:21:58 +0300

    feat: add advanced zeros function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+    let primeFactor = {};
+    let i = 2;
+    while (i <= base) {
+        if (base % i === 0) {
+            if (!primeFactor[i]){
+                primeFactor[i] = 0;
+            }
+            primeFactor[i] ++;
+            base /= i;
+        }
+        else {
+            i++;
+        }
+    }
+
+    let mass = [];
+    for (let key in primeFactor) {
+        let prime = +key;
+        let div = 2, sum = 0;
+        while (div > 1) {
+            div = parseInt(number / prime);
+            sum += div;
+            prime *= +key;
+        }
+        sum /= primeFactor[key];
+        mass.push(parseInt(sum));
+    }
+
+    let min = mass[0];
+    for (let i = 0; i < mass.length; i++) {
+        if (mass[i] < min) {
+            min = mass[i];
+        }
+    }
+
+    return min;
+};
